d5f2e714c608d9587108f5d90fa068426b8a8c77
Igor Cacerez <igor.cacerez@gmail.com>
2022-11-05 15:20:06 -0300

    localhost

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const apiUrl = "http://127.0.0.1:8000/api/v1/";
+const apiUrl = "http://localhost:8001/api/v1/";
 const TOKEN_NAME = 'sanctun';
 
 export const Http = Axios.create({
